fc60ebbfdd9deca126614bc4f25bea1478097a58
Dominick Houston <dominick.houston14@gmail.com>
2017-03-28 10:55:31 -0400

    done

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import Search from './Search';
+import SearchContainer from './SearchContainer';
+
+
+class Home extends Component {
+  render() {
+    return(
+      <div>
+        <h2>Welcome to the Movies Boi!</h2>
+        <h3><a href="/">OMDB React</a></h3>
+        <SearchContainer />
+      </div>
+    )
+  }
+}
+
+// <Search />
+
+export default Home
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+import Home from './Home';
+// import Search from './Search';
+import './index.css';
+
+ReactDOM.render(
+  <Home />,
+  document.getElementById('root')
+);
+
+// ReactDOM.render (
+//   <Search />,
+//   document.getElementById('searchMovie')
+// );
